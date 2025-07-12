fb8fb453c010ce96e19141f98733981ab612681d
Taskeren <r0yalist@outlook.com>
2025-07-12 17:13:49 +0800

    Do some chores
    - Replace the LICENSE in the resources
    - Disabled the mixin module and added unimixins as dependencies
    - Added icon
    - Renamed from ConditionMixin to Conditional Mixin Legacy
    - Modified some other credits
    - Added unimixins as requirements

--- src/main/java/me/fallenbreath/conditionalmixin/ConditionalMixinMod.java
+++ src/main/java/me/fallenbreath/conditionalmixin/ConditionalMixinMod.java
@@ -5,11 +5,16 @@ import org.apache.logging.log4j.Logger;
 
 import cpw.mods.fml.common.Mod;
 
-@Mod(modid = ConditionalMixinMod.MOD_ID_FORGE, name = "Condition Mixin", version = Tags.VERSION)
+@Mod(
+    modid = ConditionalMixinMod.MOD_ID,
+    name = ConditionalMixinMod.NAME,
+    version = Tags.VERSION,
+    dependencies = ConditionalMixinMod.DEPS)
 public class ConditionalMixinMod {
 
-    public static final String MOD_ID_FABRIC = "conditional-mixin";
-    public static final String MOD_ID_FORGE = "conditional_mixin";
+    public static final String MOD_ID = "conditional_mixin";
+    public static final String NAME = "Condition Mixin Legacy";
+    public static final String DEPS = "required-after:unimixins;";
 
     public static final Logger LOGGER = LogManager.getLogger(ConditionalMixinMod.class);
 }
--- src/main/java/me/fallenbreath/conditionalmixin/mixins/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * Well. This mod itself doesn't contain any mixins for now.
- * This package is here only for the goddamn GTNHGradle, who checks the existence of this package.
- */
-package me.fallenbreath.conditionalmixin.mixins;
